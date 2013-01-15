995d65438006c1900e334d8159d4d8a17976e292
David Klaftenegger <david.klaftenegger@it.uu.se>
2013-01-15 16:26:29 +0100

    removed wrong and unused code

--- testmap.cpp
+++ testmap.cpp
@@ -7,15 +7,6 @@
  */
 
 extern "C" {
-int compare(Eterm*, Eterm*);
-
-KVSet* new_cppset() {
-	typedef Eterm* K;
-	typedef standard_functions<K, void*, compare, malloc_function, free_function> F;
-	typedef testmap<K,F> M;
-	return make_kv_set<M>();
-}
-
 KVSet* new_cppset_default() {
 	return make_kv_set<testmap>();
 }
